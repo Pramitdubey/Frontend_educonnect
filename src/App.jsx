733386e4378914a40ecf8800101e454bbc1b9479
Pramitdubey <pramitkumardub@gmail.com>
2024-02-07 03:04:14 +0530

    updated frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Feature from "./Components/Feature.jsx";
-import Login from "./Components/Login.jsx";
-import Pricing from "./Components/Pricing.jsx";
-import About from "./Components/About.jsx";
-import Home from "./Components/Home.jsx";
+import Login from "./Pages/Login.jsx";
+import Pricing from "./Pages/Pricing.jsx";
+import About from "./Pages/About.jsx";
+import Home from "./Pages/Home.jsx";
+import SignUp from "./Pages/Signup.jsx";
+import Course from "./Pages/Courses.jsx";
+import Profile from "./Pages/Profile.jsx";
+import Coursecontent from "./Pages/Coursecontent.jsx"
+import Profilementors from "./Pages/Profilementors.jsx";
+import Profileedit from "./Pages/Profileedit.jsx";
+
 
 
 
@@ -16,11 +22,19 @@ function App() {
     
     
     <Routes>
-        <Route path="/" element={<Home/>} index/>
-        <Route path="/feature" element={<Feature/>}/>
+        <Route path="/" exact element={<Home/>} />
+        
+
+        <Route path="/courses" element={<Course/>}/>
+        <Route path="/courses/coursecontent" element={<Coursecontent/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/profile/mentors" element={<Profilementors/>}/>
+        <Route path="/profile/edit" element={<Profileedit/>}/>
         <Route path="/pricing" element={<Pricing/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+
     </Routes>
 
     </BrowserRouter>
---
+++ src/Pages/Courses.jsx
@@ -0,0 +1,190 @@
+import React from 'react'
+import { Container,Grid,Card,CardMedia,CardActions,CardContent,Button,Typography, Box,Divider} from '@mui/material'
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Navbar from '../Components/Navbar'
+import { useNavigate } from 'react-router-dom';
+import {Carousel} from 'react-responsive-carousel';
+
+
+
+const cards=[1,2,3,4,5,6,]
+
+const subjects = ['Math', 'English', 'Science', 'Economics','Computer Programming','Psychology','Business Administration','Pre-Med',];
+
+
+function Course() {
+  const navigate=useNavigate();
+  
+  return (
+    <div>
+      
+        {/* Navbar */}
+
+        <Navbar/>
+
+         {/* Heading */}
+         <main>
+        <Box sx={{
+    bgcolor: 'background.paper',
+    pt: 8,
+    pb: 6,
+    display: 'flex', flexDirection: 'column', alignItems: 'center'
+  }} >
+    
+    <Container maxWidth="lg" sx={{backgroundColor:'#13274F',borderRadius:"10px",height:100}}>
+  <Typography variant="h2" align="center" color="#F5F5F5" gutterBottom>
+    Courses
+  </Typography>
+ 
+</Container>
+    
+
+
+
+{/* Accordion */}
+<Box sx={{mt: 4}}>
+<Container maxWidth="lg" sx={{backgroundColor:'#0066b2',borderRadius:"10px",height:40}}>
+<Typography variant="h5" align="center" color="#FFFFFF" paragraph>
+    Course Catalog  
+  </Typography>
+  </Container>
+
+
+    {subjects.map((subject)=>(
+      <Accordion key={subject}>
+       <AccordionSummary
+        expandIcon={<ExpandMoreIcon />
+      }
+      sx={{
+        fontFamily: 'Arial',
+        
+      }} 
+      >
+        {subject}
+      </AccordionSummary>
+      <AccordionDetails  sx={{
+        fontFamily: 'Arial',
+        
+      }} >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        malesuada lacus ex, sit amet blandit leo lobortis eget.
+      </AccordionDetails>
+      <AccordionActions sx={{ justifyContent: 'flex-start' }}>
+        <Button variant='contained' onClick={()=>{navigate("/courses/coursecontent")}}>Visit Course</Button>
+       
+      </AccordionActions>
+    </Accordion>
+
+    ))}
+      
+      
+      </Box>
+      </Box>
+
+      <Divider sx={{ mt: 2 }}/>
+
+        {/* Cards */}
+        <Container sx={{py: 10}} maxWidth="md" >
+        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+    Trending Courses  
+  </Typography>
+
+  <Container maxWidth="lg">
+      <Carousel showArrows={true} emulateTouch={true} autoPlay={true} infiniteLoop={true} slidesToShow={2}>
+      <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                 <CardMedia 
+                 component="div"
+                 sx={{
+                   // 16:9
+                   pt: '56.25%',
+                 }}
+                 image="http://source.unsplash.com/random"
+                 title="Image title"
+                 />
+                 <CardContent sx={{ flexGrow: 1 }}>
+                   <Typography gutterBottom variant="h5">Heading</Typography>
+                   <Typography>This is cardMedia</Typography>
+                 </CardContent>
+                 <CardActions sx={{ display: 'flex', justifyContent: 'center',mb: 3 }}>
+                   
+                   <Button size="small" color="primary" variant="contained" onClick={()=>{navigate("/courses/coursecontent")}}>Visit</Button>
+                 </CardActions>
+ 
+               </Card>
+               <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                 <CardMedia 
+                 component="div"
+                 sx={{
+                   // 16:9
+                   pt: '56.25%',
+                 }}
+                 image="http://source.unsplash.com/random"
+                 title="Image title"
+                 />
+                 <CardContent sx={{ flexGrow: 1 }}>
+                   <Typography gutterBottom variant="h5">Heading</Typography>
+                   <Typography>This is cardMedia</Typography>
+                 </CardContent>
+                 <CardActions sx={{ display: 'flex', justifyContent: 'center',mb: 3 }}>
+                   
+                   <Button size="small" color="primary" variant="contained" onClick={()=>{navigate("/courses/coursecontent")}}>Visit</Button>
+                 </CardActions>
+ 
+               </Card>
+               <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                 <CardMedia 
+                 component="div"
+                 sx={{
+                   // 16:9
+                   pt: '56.25%',
+                 }}
+                 image="http://source.unsplash.com/random"
+                 title="Image title"
+                 />
+                 <CardContent sx={{ flexGrow: 1 }}>
+                   <Typography gutterBottom variant="h5">Heading</Typography>
+                   <Typography>This is cardMedia</Typography>
+                 </CardContent>
+                 <CardActions sx={{ display: 'flex', justifyContent: 'center',mb: 3 }}>
+                   
+                   <Button size="small" color="primary" variant="contained" onClick={()=>{navigate("/courses/coursecontent")}}>Visit</Button>
+                 </CardActions>
+ 
+               </Card>
+    </Carousel>
+</Container>
+        </Container>
+        </main>
+
+        <Divider sx={{ mt: 2 }}/>
+        <Box sx={{ mt: 2 }} />
+       
+       {/* Footer */}
+
+       <footer sx={{ bgcolor: 'background.paper', p: 6 }}>
+  <Typography variant="h6" align="center" gutterBottom>Footer</Typography>
+  <Grid container justifyContent="center" spacing={2}>
+    <Grid item>
+      <Button variant="text" href="#">Home</Button>
+    </Grid>
+    <Grid item>
+      <Button variant="text" href="#">About Us</Button>
+    </Grid>
+    <Grid item>
+      <Button variant="text" href="#">Contact</Button>
+    </Grid>
+  </Grid>
+  <Typography variant="body2" color="text.secondary" align="center">
+      Copyright © 
+    </Typography>
+</footer>
+      
+    </div>
+  )
+}
+
+export default Course
